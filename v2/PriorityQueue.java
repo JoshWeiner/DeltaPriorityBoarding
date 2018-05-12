b5a626764c8218ae3fda4de3169ebd50bddd9eed
Larry Wong <33496254+LarWong@users.noreply.github.com>
2018-05-12 19:35:19 -0400

    added comments for interface

--- v2/PriorityQueue.java
+++ v2/PriorityQueue.java
@@ -1,11 +1,21 @@
+
+/*****************************************************
+ * interface PriorityQueue (for strings)
+ * Specifies actions a Priority Queue must be able to perform (for Strings at the moment)
+ * Underlying container not specified.
+ *****************************************************/
 public interface PriorityQueue{
 
-    public void add(String x);
+	//add element to end of a list
+    	public void add(String x);
 
+	//traverses the list and returns the minimum (according to the standard conventions of string comparison)
 	public String peekMin();
-
+	
+	//traverses the list and removes the minimum (according to the standard conventions of string comparison)
 	public String removeMin();
 
+	//return if list is empty
 	public boolean isEmpty();
 
 
